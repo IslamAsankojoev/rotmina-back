fd8ace74512a467309263beb2b143ac0e1d04ef5
IslamAsankojoev <isla.asankojoev@mail.ru>
2025-10-21 02:50:04 +0600

    added nodemailer

--- config/plugins.ts
+++ config/plugins.ts
@@ -12,4 +12,32 @@ export default ({ env }) => ({
       deleteJwtFromResponse: true,
     },
   },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.gmail.com'),
+        port: env.int('SMTP_PORT', 465),
+        secure: env.bool('SMTP_SECURE', true),
+        auth: {
+          user: env('SMTP_USER', 'your-email@example.com'),
+          pass: env('SMTP_PASS', 'asdf asdf asdf asdf'),
+        },
+      },
+      settings: {
+        defaultFrom: env('EMAIL_FROM', 'your-email@example.com'),
+        defaultReplyTo: env('EMAIL_REPLY_TO', 'your-email@example.com'),
+      },
+    },
+  },
+  'users-permissions': {
+    config: {
+      register: {
+        allowedFields: ["surname"],
+      },
+      jwt: {
+        expiresIn: '1h',
+      },
+    },
+  },
 });
