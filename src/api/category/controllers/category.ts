bb87c5e2fa25ecb21597c4c02e69cb808e4c4d92
IslamAsankojoev <isla.asankojoev@mail.ru>
2025-10-21 22:57:51 +0600

    added category product count property

--- src/api/category/controllers/category.ts
+++ src/api/category/controllers/category.ts
@@ -4,4 +4,49 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::category.category');
+export default factories.createCoreController('api::category.category', ({ strapi }) => ({
+  async find(ctx) {
+    const { data, meta } = await super.find(ctx)
+
+    const dataWithCount = await Promise.all(
+      data.map(async (category) => {
+        // Получаем категорию с populate products
+        const categoryWithProducts = await strapi.documents('api::category.category').findOne({
+          documentId: category.documentId,
+          populate: ['products'],
+          status: 'published'
+        })
+        
+        const count = categoryWithProducts?.products?.length || 0
+        
+        return {
+          ...category,
+          count,
+        }
+      }),
+    )
+
+    return { data: dataWithCount, meta }
+  },
+
+  async findOne(ctx) {
+    const response = await super.findOne(ctx)
+    const category = response.data
+
+    // Получаем категорию с populate products
+    const categoryWithProducts = await strapi.documents('api::category.category').findOne({
+      documentId: category.documentId,
+      populate: ['products']
+    })
+    
+    const count = categoryWithProducts?.products?.length || 0
+
+    return {
+      ...response,
+      data: {
+        ...category,
+        count,
+      },
+    }
+  },
+}))
